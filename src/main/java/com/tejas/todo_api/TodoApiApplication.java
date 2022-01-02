2096d16b2d049683453a3dc636d0867e9e21d53a
kalandhar26 <babakalandhar499@gmail.com>
2022-01-02 23:00:33 +0530

    Revert "first commit"
    This reverts commit ed315a7be8f1219739af09d3c771c05749d2fb2f.

--- src/main/java/com/tejas/todo_api/TodoApiApplication.java
+++ src/main/java/com/tejas/todo_api/TodoApiApplication.java
@@ -2,7 +2,7 @@ package com.tejas.todo_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
- // main method
+
 @SpringBootApplication
 public class TodoApiApplication {
 
