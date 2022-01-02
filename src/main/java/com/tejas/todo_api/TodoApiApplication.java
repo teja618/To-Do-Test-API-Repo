721eeb72c3de2aefbbbc55f2af47ac1009b50c53
kalandhar26 <babakalandhar499@gmail.com>
2022-01-02 23:06:56 +0530

    second

--- src/main/java/com/tejas/todo_api/TodoApiApplication.java
+++ src/main/java/com/tejas/todo_api/TodoApiApplication.java
@@ -2,12 +2,11 @@ package com.tejas.todo_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+ // main method babab
 @SpringBootApplication
 public class TodoApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApiApplication.class, args);
 	}
-
 }
